3c91cc92a06eecbaa79789e0ff61f4101f3296e2
Andrés Alfaya Mariño <andres.alfaya@ctag.com>
2025-11-10 10:34:19 +0100

    Add random to custom exporter

--- custom_exporter/custom_exporter.py
+++ custom_exporter/custom_exporter.py
@@ -1,17 +1,20 @@
 from flask import Flask, Response
+import random  # <--- importar random
 
 app = Flask(__name__)
 
 @app.route('/custom-metrics')
 def custom_metrics():
-  # Custom metrics in Prometheus exposition format
-  metrics_data = """
+    # Generar un valor aleatorio entre 0 y 100
+    random_value = random.randint(0, 100)
+
+    # Custom metrics en formato Prometheus
+    metrics_data = f"""
 # HELP my_custom_metric A custom metric example.
 # TYPE my_custom_metric counter
-my_custom_metric 42
+my_custom_metric {random_value}
 """
-  return Response(metrics_data, mimetype='text/plain')
+    return Response(metrics_data, mimetype='text/plain')
 
 if __name__ == '__main__':
-  # Listen on port 8001
-  app.run(host='0.0.0.0', port=8001)
+    app.run(host='0.0.0.0', port=8001)
